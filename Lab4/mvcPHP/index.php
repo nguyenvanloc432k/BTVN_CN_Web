e67bffeaa9e170ccb4cdd3cf6439624a668e2c58
boyonepiece <stah432987@gmail.com>
2021-04-14 00:35:17 +0700

    Lab4

---
+++ Lab4/mvcPHP/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8"/>
+	<title>Lab4 Part 1</title>
+</head>
+
+<body>
+	<h1>Vi du ve mo hinh MVC</h1>
+	
+    <form action="controllers/itemscontroller.php" method="POST">
+        <input type="number" name="id"> <input type="submit" value="Search">
+    </form>
+	
+</body>
+</html>
