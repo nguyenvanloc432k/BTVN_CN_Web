6dec9843f00dfe7d46c07d37f7ea381ed94b49a3
boyonepiece <stah432987@gmail.com>
2021-04-13 11:57:07 +0700

    Lab4

---
+++ Lab4/ownMVC/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+/** Configuration Variables **/
+
+define ('DEVELOPMENT_ENVIRONMENT',true);
+
+define('DB_NAME', 'ownMVC');
+define('DB_USER', 'kahnpotter');
+define('DB_PASSWORD', 'mysql123');
+define('DB_HOST', 'localhost');
